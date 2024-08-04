8176b06b7be0cb4c37cba0e8d9942f6c925a9237
Louis Lomboy <llomboy@uw.edu>
2024-08-04 10:16:49 -0700

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+    strings: [
+        "a Frontend Developer",
+        "a Backend Developer",
+        "a Web Developer",
+        "an Artist",
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
